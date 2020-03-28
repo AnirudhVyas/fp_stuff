b93bf7b43744ba37955088078d6e235008ee70d8
AnirudhVyas <ricky.nj@gmail.com>
2020-03-27 22:20:17 -0700

    chapter 4 for fp in scala option implementation

---
+++ src/main/scala/free/me/typeclasses/redbook/Chapter4.scala
@@ -0,0 +1,31 @@
+package free.me.typeclasses.redbook
+/**
+ * Chapter 4 solutions for red book.
+ * refer to functional programming in scala book.
+ */
+object Chapter4 {
+  import scala.{Either => _, Option => _}
+  // 4.1
+  trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case Just(a) => Just(f(a))
+      case NothingM => NothingM
+    }
+    // this is taken from answer key -- couldn't figure it out by myself.
+    // the trick is what do you want to get the result type.
+    // i have map so far, if i do map(f) i get option[option]
+    // how do i get option out of it, by doing a get.
+    def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(NothingM)
+    def getOrElse[B >: A](default: => B): B = this match {
+      case Just(a) => a
+      case NothingM => default
+    }
+    def orElse[B >: A](ob: => Option[B]): Option[B] = map(Just(_)).getOrElse(ob)
+    def filter(f: A => Boolean): Option[A] = this match {
+      case Just(a) => if (f(a)) Just(a) else NothingM
+      case NothingM => NothingM
+    }
+  }
+  case object NothingM extends Option[Nothing]
+  case class Just[+A](a: A) extends Option[A]
+}
---
+++ src/test/scala/free/me/typeclasses/homegrown/ApplicativeFunctorLaws.scala
@@ -0,0 +1,13 @@
+package free.me.typeclasses.homegrown
+import org.scalacheck.Properties
+import org.scalacheck.Prop._
+/**
+ * Applicative laws are:
+ * 1.identity
+ * 2. composition
+ * 3. homomorphism
+ * 4. interchange
+ */
+class ApplicativeFunctorLaws extends Properties("ApplicativeFunctorLaws"){
+ // todo:
+}
